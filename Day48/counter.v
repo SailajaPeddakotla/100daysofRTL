7b9466eaf3db2f023badf2163e713db95f780dbe
Sailaja Peddakotla <86059139+SailajaPeddakotla@users.noreply.github.com>
2023-10-30 19:34:43 +0530

    Add files via upload

---
+++ Day48/counter.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.10.2023 22:28:44
+// Design Name: 
+// Module Name: counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module counter(
+input clk,rst,enable,
+output reg [3:0]counter_output
+);
+
+always@ (posedge clk)
+begin 
+if( rst | counter_output==4'b1001)
+counter_output <= 4'b0000;
+else if(enable)
+counter_output <= counter_output + 1;
+else
+counter_output <= 0;
+end
+endmodule
